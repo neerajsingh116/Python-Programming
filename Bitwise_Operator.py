d31c9624dacef94c10c57ad1e18d367e943e9580
Neeraj Singh <55570495+neerajsingh116@users.noreply.github.com>
2020-02-03 23:25:15 +0530

    Create Bitwise_Operator.py

---
+++ Bitwise_Operator.py
@@ -0,0 +1,5 @@
+a=6       #values first get converted into binary then bitwise operation will be performed
+b=2
+print("bitwise and =",a & b)    
+print("bitwise or =",a | b)
+print("bitwise xor =",a ^ b)
